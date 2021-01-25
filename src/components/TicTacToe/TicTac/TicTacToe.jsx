6c4d37f7892d27790e3621e78ad7abf44c7919f2
Kirill <kirill_94_@mail.ru>
2021-01-25 17:53:28 +0300

    'ticTacToe_victoryCheck'

--- src/components/TicTacToe/TicTac/TicTacToe.jsx
+++ src/components/TicTacToe/TicTac/TicTacToe.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './TicTacToe.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTimes } from '@fortawesome/free-solid-svg-icons'
@@ -6,7 +6,7 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons'
 let times = <FontAwesomeIcon className={styles.times} icon={faTimes} />
 let circle = <div className={styles.circle}></div>
 
-let winningCombination = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 6], [3, 5, 7]]
+let winningCombination = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
 
 const TikTakToe = () => {
     let stateArr = [{ id: 1, times: false, circle: false },
@@ -21,18 +21,54 @@ const TikTakToe = () => {
 
 
     let [state, setState] = useState(stateArr);
-    let [priorityCircle, setPriorityCircle] = useState(true);
+    let [priorityCircle, setPriorityCircle] = useState(false);
     let [combinationX, setCombinationX] = useState([]);
     let [combinationO, setCombinationO] = useState([]);
+    let [winnerIndicator, setWinnerIndicator] = useState(false);
+
+    useEffect(() => {
+        if (combinationX.length >= 3 || combinationO.length >= 3) victoryCheck()
+    })
+
+    let clearFild = () => {
+        debugger
+        setTimeout(() => {
+            setState(stateArr)
+            setCombinationX([])
+            setCombinationO([])
+            setWinnerIndicator(false)
+            setPriorityCircle(false)
+        }, 3000);
+    }
+
+    const victoryCheck = () => {
+        winningCombination.forEach((itemWinArr) => {
+            console.log('fwefwe')
+            let winnerX = 0
+            let winnerO = 0
+            itemWinArr.forEach(itemWin => {
+                if (combinationX.includes(itemWin)) winnerX++
+                if (combinationO.includes(itemWin)) winnerO++
+            })
+            if (winnerX == 3) {
+                setWinnerIndicator(true)
+                clearFild()
+            }
+            if (winnerO == 3) {
+                setWinnerIndicator(true)
+                clearFild()
+            }
+            winnerX = 0;
+            winnerO = 0;
+        })
+    }
 
     let onClickZone = (e) => {
-        let stateCopy = state.map(item => {
+        state.map(item => {
             if (e.target.id == item.id && item.times == false && item.circle == false) {
                 if (priorityCircle) {
-                    debugger
                     item.circle = true
                     setCombinationO([...combinationO, item.id])
-                    console.log(combinationO)
                     setPriorityCircle(false)
                 } else {
                     item.times = true
@@ -42,13 +78,18 @@ const TikTakToe = () => {
             }
             return item
         })
-        setState(stateCopy);
     }
+
     let renderState = state.map(item => {
         return (
-            <div className={styles.zone} id={item.id} key={item.id} onClick={onClickZone}>
-                {item.times && times}
-                {item.circle && circle}
+            <div className={styles.container}>
+                <div className={styles.zone + " " + (winnerIndicator && styles.zoneWiner)} id={item.id} key={item.id} onClick={!winnerIndicator && onClickZone}>
+                    {item.times && times}
+                    {item.circle && circle}
+                </div>
+                <div className={styles.zone + " " + styles.zoneAbsolute }>
+
+                </div>
             </div>
         )
     })
