5934e82ab28578bd5510f9fac0864bb7b1d1b7be
Shlkirill <kirill_94_@mail.ru>
2021-02-14 14:15:11 +0300

    colorButton

--- src/components/Request/Main/BlockInfo/BlockInfo.jsx
+++ src/components/Request/Main/BlockInfo/BlockInfo.jsx
@@ -1,17 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './BlockInfo.module.css'
 
-const BlockInfo = ({ id, postBody, postTitle }) => {
+const BlockInfo = ({ id, postBody, postTitle, onViewPost }) => {
+    let [backgroundColor, setBackgroundColor] = useState(0)
+
+    const onSetBackgroundColor = () => {
+        setBackgroundColor(backgroundColor = backgroundColor + 1)
+        if (backgroundColor == 4)  setBackgroundColor(0)
+    }
+
     return (
-        <div className={styles.blockInfo}>
+        <div className={styles.blockInfo
+            + ' ' + (backgroundColor == 1 && styles.backgroundRed)
+            + ' ' + (backgroundColor == 2 && styles.backgroundGreen)
+            + ' ' + (backgroundColor == 3 && styles.backgroundBlue)}>
             <div className={styles.blockInfo_container}>
                 <div className={styles.blockInfo_wrapper}>
                     <h3 className={styles.blockInfo_tittle}>{postTitle}</h3>
                     <p className={styles.blockInfo_info}>{postBody}</p>
                 </div>
                 <div className={styles.blockInfo_navigation}>
-                    <button>Больше</button>
-                    <button>Еще</button>
+                    <button onClick={() => onViewPost(postTitle, postBody)}>Больше</button>
+                    <button onClick={onSetBackgroundColor}>Цвет</button>
                 </div>
             </div>
         </div>
--- src/components/Request/Main/Main.jsx
+++ src/components/Request/Main/Main.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {useTransition, animated} from 'react-spring'
+import { useTransition, animated } from 'react-spring'
 import { useState } from 'react/cjs/react.development'
 import BlockInfo from './BlockInfo/BlockInfo'
 import styles from './Main.module.css'
@@ -7,7 +7,30 @@ import styles from './Main.module.css'
 const Main = ({ postsList }) => {
 
     let [amountPosts, setAmountPosts] = useState(3)
-    
+    let [viewPost, setViewPost] = useState({
+        title: '',
+        body: '',
+        show: false
+    })
+    const onShowMore = () => {
+        setAmountPosts(amountPosts + 3)
+    }
+    const onViewPost = (title, body) => {
+
+        setViewPost({
+            ...viewPost,
+            title,
+            body,
+            show: true
+        })
+    }
+    const onCloseViewPost = () => {
+        setViewPost({
+            ...viewPost,
+            show: false
+        })
+    }
+    console.log(viewPost)
     const transitions = useTransition(postsList, item => item.id, {
         from: { transform: 'translate3d(0,-40px,0)' },
         enter: { width: '300px' },
@@ -15,25 +38,31 @@ const Main = ({ postsList }) => {
     })
     let a = transitions.map(({ item, props, key }) => {
 
-    if (item.id <= amountPosts)    return <animated.div >
-            <BlockInfo key={key} style={props} id={item.id} postBody={item.body} postTitle={item.title} />
+        if (item.id <= amountPosts) return <animated.div >
+            <BlockInfo key={key} style={props} id={item.id} postBody={item.body}
+                postTitle={item.title} onViewPost={onViewPost} />
         </animated.div>
     })
-
     const onePost = postsList.map(item => {
         if (item.id <= amountPosts) return <BlockInfo key={item.id} id={item.id} postBody={item.body} postTitle={item.title} />
     })
 
-    const onShowMore = () => {
-        setAmountPosts(amountPosts + 3)
-    }
     return (
         <div className={styles.main}>
             <div className={styles.main_container}>
                 {a}
             </div>
-            <div className={styles.main_showMoreContainer}>
+            <div className={a[a.length-1] == undefined ? styles.main_showMoreContainer : styles.hide}>
                 <button className={styles.main_showMore} onClick={onShowMore}>Show more</button>
+            </div>
+            <div className={viewPost.show ? styles.viewModal : styles.hide}>
+                <div className={styles.viewModal_wrapper}>
+                    <h3>{viewPost.title}</h3>
+                    <p>{viewPost.body}</p>
+                    <div className={styles.viewModal_navigation}>
+                        <button onClick={onCloseViewPost}>Close</button>
+                    </div>
+                </div>
             </div>
         </div>
     )
