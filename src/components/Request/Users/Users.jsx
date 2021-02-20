46e8f6e7f6c2f87af16658d927cb6d5da5b2d71a
Kirill <kirill_94_@mail.ru>
2021-02-20 14:12:39 +0300

    moreusers

--- src/components/Request/Users/Users.jsx
+++ src/components/Request/Users/Users.jsx
@@ -1,18 +1,26 @@
 import React from 'react'
+import { useState } from 'react/cjs/react.development'
 import UserCard from './UserCard/UserCard'
 import styles from './Users.module.css'
 
 const Users = ({ users }) => {
+    let [visibleUsers, setVisibleUsers] = useState(3)
+    let userList = users.map((item, index) => {
 
-    let userList = users.map(item => {
-        return <UserCard key={item.id} id={item.id} name={item.name} userName={item.username}
-                        phone={item.phone} website={item.website} email={item.email} 
-                        address={item.address} company={item.company}/>
+
+        if (index < visibleUsers) return <UserCard key={item.id} id={item.id} name={item.name} userName={item.username}
+            phone={item.phone} website={item.website} email={item.email}
+            address={item.address} company={item.company} />
     })
     return (
         <div className={styles.container}>
             <div className={styles.wrapperUserList}>
-                {userList}
+                <div className={styles.userList}>
+                    {userList}
+                </div>
+                <div>
+                    <button onClick={() => {setVisibleUsers(visibleUsers + 3)}}>More</button>
+                </div>
             </div>
         </div>
     )
