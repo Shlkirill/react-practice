c0dfcd0e04dc72303ecfaac7135a57773fd1b965
Shlkirill <kirill_94_@mail.ru>
2021-02-27 17:16:46 +0300

    deleteSlickslider

--- src/components/Request/Photos/PhotoGallery/PhotoGallery.jsx
+++ src/components/Request/Photos/PhotoGallery/PhotoGallery.jsx
@@ -4,29 +4,17 @@ import Slider from 'react-slick'
 
 const PhotoGallery = (props) => {
 
-    const [sliderSettings, setSliderSettings] = useState({
-        infinite: false,
-        speed: 500,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        arrows: true,
-        centerMode: true
-    })
 
     let arrPhotosList = props.photosList.map(item => {
         return <div className={styles.photo}>
             <img src={item.url} alt="" />
         </div>
     })
-
-    console.log(arrPhotosList)
     return (
         <div className={styles.container}>
             <div className={styles.wrapper}>
                 <div className={styles.photos}>
-                    <Slider {...sliderSettings}>
-                            {arrPhotosList}
-                    </Slider>
+                    {arrPhotosList}
                 </div>
             </div>
         </div>
