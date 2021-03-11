f771adc136f296e0960a6308b8442b0ec67b1915
Kirill <kirill_94_@mail.ru>
2021-03-11 17:21:09 +0300

    photosIcons

--- src/components/Request/Photos/PhotoGallery/BigPhoto/BigPhoto.jsx
+++ src/components/Request/Photos/PhotoGallery/BigPhoto/BigPhoto.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import styles from './BigPhoto.module.css'
-import { faTimes } from '@fortawesome/free-solid-svg-icons'
+import { faEdit, faTimes, faTrash } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const BigPhoto = (props) => {
@@ -12,18 +12,28 @@ const BigPhoto = (props) => {
         if (target.id == 1) props.historyUrl.push(`/request_axios/photos/${props.idAlbum}`)
     }
 
-    const deletePhoto = <FontAwesomeIcon icon={faTimes} className={styles.iconDelete} onClick={()=> {}} />
+    const closeWindow = <FontAwesomeIcon icon={faTimes} className={styles.closeWindow} onClick={() => { }} />
+    const editTitle = <FontAwesomeIcon icon={faEdit} className={styles.iconEdit} />
+    const deletePhoto = <FontAwesomeIcon icon={faTrash} className={styles.iconDelete} />
+
     return (
         <div className={styles.container} id='1' onClick={(e) => { onRedirect(e.target) }}>
             <div className={styles.wrapper}>
                 <div>
                     <button onClick={() => { props.historyUrl.push(`/request_axios/photos/${props.idAlbum}/${+props.idPhoto - 1}`) }}>Back</button>
                 </div>
-                <div className={styles.modal}>
-
-                        <img src={photo.url} alt="" />
+                <div className={styles.modalPhoto}>
+                    <div className={styles.modalPhoto__wrapper}>
+                        <img className={styles.photo} src={photo.url} alt="" />
+                    </div>
+                    <div className={styles.modalPhoto__navigationWrapper}>
                         <p className={styles.title}>{photo.title}</p>
-                    {deletePhoto}
+                        <div className={styles.modalPhoto__navigation}>
+                            {editTitle}
+                            {deletePhoto}
+                        </div>
+                    </div>
+                    {closeWindow}
                 </div>
                 <div>
                     <button onClick={() => { props.historyUrl.push(`/request_axios/photos/${props.idAlbum}/${+props.idPhoto + 1}`) }}>onward</button>
